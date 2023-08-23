93309da8e2029b8d7b025585a79fd4b6e9ffe6bb
netarnort <nogascarma1234@gmail.com>
2023-08-24 06:29:00 +0700

    midterm

--- server/src/controllers/NodeJS-midterm66.js
+++ server/src/controllers/NodeJS-midterm66.js
@@ -6,4 +6,28 @@ module.exports = {
         res.send('ผลลัพท์ :' + result);
     },
 
+    que2(req,res){
+        X = parseInt(JSON.stringify(req.body.input1));
+        Y = parseInt(JSON.stringify(req.body.input2));
+        // X = parseInt(JSON.stringify(req.body.['input1']));
+        // Y = parseInt(JSON.stringify(req.body.['input2']));
+      
+
+        for(i=X+Y;i>0;i--)
+        {
+            if(Y%i==0 && X%i==0){
+                gcd=i;
+                break;
+            }
+        }
+        
+        mid=(X*Y)
+        if(mid<0){
+            mid=mid*-1
+        }
+        result = mid/gcd
+        res.send('ผลลัพท์ :' + ' ' + result.toString())
+    }
+
+
 };
--- server/src/route.js
+++ server/src/route.js
@@ -3,5 +3,6 @@ const NodeJS_midterm66 = require('./controllers/NodeJS-midterm66')
 module.exports = (app) =>{
 
     app.post('/que1', NodeJS_midterm66.que1)
+    app.post('/que2', NodeJS_midterm66.que2)
 
 }
