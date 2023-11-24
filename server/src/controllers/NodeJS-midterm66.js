f6b3dd63598da8caf7bab00af39a42b94569d1f3
netarnort <nogascarma1234@gmail.com>
2023-11-24 13:58:16 +0700

    midterm

--- server/src/controllers/NodeJS-midterm66.js
+++ server/src/controllers/NodeJS-midterm66.js
@@ -2,32 +2,26 @@ module.exports = {
 
     que1(req, res) {
         x = parseInt(JSON.stringify(req.body.input1));
-        result = x % 2;
+        result = x * x;
         res.send('ผลลัพท์ :' + result);
     },
 
-    que2(req,res){
+    que2(req, res) {
         X = parseInt(JSON.stringify(req.body.input1));
         Y = parseInt(JSON.stringify(req.body.input2));
-        // X = parseInt(JSON.stringify(req.body.['input1']));
-        // Y = parseInt(JSON.stringify(req.body.['input2']));
-      
 
-        for(i=X+Y;i>0;i--)
-        {
-            if(Y%i==0 && X%i==0){
-                gcd=i;
+        for(i = X + Y; i > 0; i--) {
+            if(Y % i == 0 && X % i == 0) {
+                gcd = i;
                 break;
             }
         }
         
-        mid=(X*Y)
-        if(mid<0){
-            mid=mid*-1
+        mid = X * Y;
+        if(mid < 0) {
+            mid = mid * -1;
         }
-        result = mid/gcd
-        res.send('ผลลัพท์ :' + ' ' + result.toString())
+        result = mid / gcd;
+        res.send('ผลลัพท์ :' + ' ' + result.toString());
     }
-
-
 };
